644e63fd3bec9f7b4a4a036b752be20ba815f387
Calvin Ting <calvinting@protonmail.com>
2018-02-19 15:35:38 -0800

    first WP commit

---
+++ themes/red-theme/footer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+			</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="footer-container">
+					<div class="contact">
+					<h3>Contact info </h3>
+					<p><i class="fas fa-camera-retro"></i> info@inhabitent.com</p>
+					<p>778-456-7891</p>
+					</div>
+					<div class="bhours">
+					<h3>Business Hours</h3>
+					<p><span class="daysweek">Monday - Friday: </span> 9am to 5pm</p>
+					<p><span class="daysweek">Saturday: </span> 10am to 2pm</p>
+					<p><span class="daysweek">Sunday: </span> Closed</p>
+					</div>
+					<div class="textlogo">
+					<img src="http://tent.academy.red/wp-content/themes/inhabitent/images/inhabitent-logo-text-dark.svg" alt="footerlogo">
+					</div>
+					</div>
+					<div class="site-info">
+					<p>COPYRIGHT © 2017 INHABITENT</p>
+				</div><!-- .site-info -->
+				</div>
+			</footer><!-- #colophon -->
+		</div><!-- #page -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
